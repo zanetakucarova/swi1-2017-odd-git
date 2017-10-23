bf6aa5f1b6379eaa73515ac39e3522d37977fb5f
Martin Hradil <mhradil@redhat.com>
2017-10-23 15:35:36 +0000

    script to copy the tmp instructions

---
+++ skript.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for f in tombajtek     davidbetko     xcocul00     doruskova     glmcz     AceLiver     xhonek3     xjakl     lkaplanov     JakubKubista     zanetakucarova     necik11     PeterOsusky     crinos9     xpolako3     DavidPrexta     webe745     tveskrna lasko ; do
+	cp tmp "$f".md
+done
